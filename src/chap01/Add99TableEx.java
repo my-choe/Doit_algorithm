087487fd4fc76da99b4cb037cb36b2d80efb7f02
minyoung choi <my_choe@naver.com>
2020-02-28 23:45:00 +0900

    위쪽과 왼쪽에 더하는 수가 있는 덧셈표

---
+++ src/chap01/Add99TableEx.java
@@ -0,0 +1,22 @@
+package chap01;
+
+public class Add99TableEx {
+	// 위쪽과 왼쪽에 곱하는 수가 있는 곱셈표 출력
+	public static void main(String[] args) {
+		
+		System.out.print("    |");
+		for(int i = 1; i <= 9;i++) {
+			System.out.printf("%5d", i);
+		}
+		System.out.println("\n----+---------------------------------------------");
+		
+		for(int j = 1; j<= 9; j++) {
+			System.out.print("  " + j + " |");
+			for(int k = 1; k <= 9; k++) {
+				System.out.printf("%5d", j+k);
+			}
+			System.out.println();
+		}
+	}
+
+}
